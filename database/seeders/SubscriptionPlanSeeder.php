ec5d57c648a1b65768940b0f9f30eb42d6001e54
kerolkimie <hakimisaidi@yahoo.com>
2026-08-20 16:02:22 +0800

    try edit price basic

--- database/seeders/SubscriptionPlanSeeder.php
+++ database/seeders/SubscriptionPlanSeeder.php
@@ -12,7 +12,7 @@ class SubscriptionPlanSeeder extends Seeder
         $plans = [
             [
                 'name' => 'Basic',
-                'price' => 70000,
+                'price' => 70000000,
                 'points_included' => 0,
                 'duration_days' => 30,
                 'max_branches' => 1,
